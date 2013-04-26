9f13b3dfb9636f73b7c66cfb40499089e545835e
Landon Schropp <schroppl@gmail.com>
2013-04-26 00:52:36 -0700

    Revamped the application structure.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,8 +1,15 @@
+// libraries
+
 //= require libraries/jquery-1.9.1
 //= require libraries/handlebars-1.0.0-rc.3
 //= require libraries/ember-1.0.0-rc.3
 
+// application
+
 //= require_self
+//= require router
+//= require_tree ./routes
+//= require_tree ./controllers
 //= require_tree ./templates
 
 (function($) {
