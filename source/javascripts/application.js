f4be53f5ebc081cb75ee59b1c685df2854b0f80f
Landon Schropp <schroppl@gmail.com>
2013-05-02 23:58:44 -0700

    Fixed the background problem in mobile Safari.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -16,6 +16,16 @@
 
 (function($) {
 
+  // BUG FIX: This is part of a bug fix that detects if the device is running iOS. Normally, user
+  // agent sniffing would be considered a bad idea, because it doesn't reliably detect if a feature
+  // is implemented. However, in this case the problem stems from a bug with mobile Safari, which
+  // can't be detected through feature detection.
+  var ios = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+
+  // append a class to the html element if the) device is an iOS device
+  if (ios)
+    $("html").addClass("ios");
+
   // start the application
   window.Parachute = Ember.Application.create();
 
